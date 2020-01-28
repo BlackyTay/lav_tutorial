085dd5c6f91dd71c5868f2b57f736d2b3eb250ab
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-28 14:58:47 +0800

    Modify products.edit to move field position

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -46,14 +46,14 @@
                     <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Publish:</strong>
                     <input name="publish" type="checkbox" class="form-check-input" value="1" {{ $product->publish ? 'checked' : '' }}
                 </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
    
