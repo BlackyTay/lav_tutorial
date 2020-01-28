509e17c9fe93bb5ad9f33b9bac1b19ae6411bb52
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-28 14:51:09 +0800

    Modify products.edit to enhance view

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -37,7 +37,7 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Price:</strong>
-                    <textarea class="form-control" style="height:150px" name="price" placeholder="Price">{{ $product->price }}</textarea>
+                    <input type="number" min="0" step=".01" name="price" value="{{ $product->price }}" class="form-control" placeholder="Price">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
