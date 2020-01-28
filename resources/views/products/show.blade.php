e3591431a07761f324b7066bfae47ebf8a817a0e
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-28 14:49:22 +0800

    Modify products.show to show publish in YES/NO

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -35,7 +35,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Publish:</strong>
-                {{ $product->name }}
+                {{ $product->public == 0 ? 'YES' : 'NO' }}
             </div>
         </div>
     </div>
